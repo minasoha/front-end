e11c8e47098bc271804b6796c76dcfdad6bef497
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-18 11:54:08 -0700

    updated global theme and styled cover page

--- src/components/pages/CoverPage/CoverPage.jsx
+++ src/components/pages/CoverPage/CoverPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const CoverPage = () => {
   return (
-    <section data-testid="cover-page">
+    <section data-testid="cover-page" className="cover-page">
       <h1 className="cover-page__title">Potluck Planner</h1>
       <h2 className="cover-page__subtitle">
         Your one-stop shop for all things potluck.
