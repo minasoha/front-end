dcb2a94da5e2882327ef0a2a2c7ce141799baae4
minasoha <minasoha@yahoo.com>
2021-10-18 18:43:40 -0400

    basic dashboardPage and CreatePotluckPage

---
+++ src/components/pages/CreatePotluckPage/CreatePotluckPage.js
@@ -0,0 +1,47 @@
+import React , { useEffect } from "react";
+import { useHistory } from "react-router";
+// import axiosWithAuth from "../../../utilities/axiosWithAuth";
+
+const CreatePotluckPage = () => {
+    const { push } = useHistory();
+    // useEffect(
+    //     axiosWithAuth().post('')
+    //         .then(resp => {
+    //             console.log(resp)
+    //         }).catch(err=>{console.log(err)})
+    // )
+    const handleSubmit = e => {
+        e.preventDefault();
+        push('/dashboard')
+    }
+    return(
+        
+            
+                
+            
+                <form className="creat-potluck" onSubmit={handleSubmit}>
+                    <h1>Creat Potluck</h1>
+                    <label>
+                        Event Name:
+                        <input type="text" name="event-name" />
+                    </label>
+                    <label>
+                        Location:
+                        <input type="text" name="location" />
+                    </label>
+                    <label>
+                    Date/Time:
+                    <input type="datetime-local" name="dateTime" />
+                    </label>
+
+                    <button onSubmit={handleSubmit} className="cover-page__cta-button">Creat Potluck</button>
+                </form>
+
+            
+    
+
+
+    );
+};
+
+export default CreatePotluckPage;
--- src/components/pages/LoginPage/LoginPage.jsx
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
-
+import CreatePotluckPage from "../CreatePotluckPage/CreatePotluckPage";
 export const LoginPage = () => {
   return (
     <section data-testid="login-page" className="login-page">
       <div className="login-page__form-selector">
+
+      <CreatePotluckPage/>
         <NavLink
           data-testid="login-page__signup-button"
           className="login-page__form-select-button"
