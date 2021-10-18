94f5869761d3de2bd8c5d91bff440b873cc4b8f1
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-18 13:24:47 -0700

    removed Redux, reducers, actions; added context for login to App

--- src/index.jsx
+++ src/index.jsx
@@ -2,28 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-// Reducers
-import { reducer } from "./reducers";
 // Components
 import App from "./App";
 // Styles
 import "./styles/index.css";
 
-// Create redux store (w/ logger + thunk middleware)
-const reduxStore = createStore(reducer, applyMiddleware(logger, thunk));
-
 // Render the frontend (in strict mode w/ SPA Routing + Redux)
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={reduxStore}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
