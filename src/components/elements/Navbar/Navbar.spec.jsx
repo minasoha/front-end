57d36d1b2e189dab2b4c5aa2314a0dcfb17157cb
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-20 15:33:11 -0700

    added testing for navbar logout functionality

--- src/components/elements/Navbar/Navbar.spec.jsx
+++ src/components/elements/Navbar/Navbar.spec.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithRouter } from "./../../../utilities";
 import { Navbar } from "./Navbar";
 import { LoginContext } from "./../../../contexts";
@@ -15,5 +16,31 @@ describe("Navbar Component", () => {
       </LoginContext.Provider>,
       true
     );
+  });
+
+  it("can log a user out when logout button is pressed", () => {
+    // Set up local storage
+    localStorage.clear();
+    localStorage.setItem("token", "testToken");
+
+    // Mock global state
+    const setIsLoggedIn = jest.fn();
+
+    // Render the Navbar
+    renderWithRouter(
+      <LoginContext.Provider value={{ setIsLoggedIn }}>
+        <Navbar />
+      </LoginContext.Provider>,
+      true
+    );
+
+    // Act: click the logout button
+    const logoutButton = screen.getByTestId("navbar__logout-link");
+    userEvent.click(logoutButton);
+
+    // Assert: is login token gone, and login page current page?
+    const loginToken = localStorage.getItem("token");
+    expect(loginToken).toBeNull();
+    expect(location.pathname).toEqual("/login");
   });
 });
