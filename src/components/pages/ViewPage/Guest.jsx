749110dd817186a6d80323e63e201091b5aeab4f
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-21 13:42:15 -0700

    guests show up on view page as guest

--- src/components/pages/ViewPage/Guest.jsx
+++ src/components/pages/ViewPage/Guest.jsx
@@ -1,24 +1,57 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { LoginContext } from "../../../contexts";
+import { axiosWithAuth } from "../../../utilities";
 
 export const Guest = () => {
-  // try {
-  //   axios
-  //     .get("")
-  //     .then((resp) => {
-  //       console.log(resp.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // } catch (err) {
-  //   console.error("An error has occurred:", err);
-  // }
+  // Destructuring
+  const { user_id } = useContext(LoginContext);
+  const { potluck_id } = useParams();
+
+  // State
+  const [currentItems, setCurrentItems] = useState([]);
+  const [guests, setGuests] = useState([]);
+
+  // Helpers
+  const updateCurrentItems = async () => {
+    try {
+      const databaseItems = await axiosWithAuth().get(
+        `https://potluckplanner-bw-10-2021.herokuapp.com/api/potluck/items/${user_id}/${potluck_id}`
+      );
+      setCurrentItems(databaseItems.data);
+    } catch (error) {
+      console.error("could not fetch current items", error);
+    }
+  };
+  const updateGuests = async () => {
+    try {
+      const guestList = await axiosWithAuth().get(
+        `https://potluckplanner-bw-10-2021.herokuapp.com/api/potluck/guests/${user_id}/${potluck_id}`
+      );
+      setGuests(guestList.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Side Effects
+  useEffect(() => {
+    updateCurrentItems();
+    updateGuests();
+  }, []);
+  useEffect(() => {}, [currentItems, guests]);
 
   return (
-    <section className="guests">
+    <section>
       <div>
-        <input type="checkbox" />
+        <h3>Current Items</h3>
+        {currentItems.map((item) => {
+          return <p>{item.item}</p>;
+        })}
+        <h3>Current Guests</h3>
+        {guests.map((guest) => {
+          return <p>{guest.username}</p>;
+        })}
       </div>
     </section>
   );
