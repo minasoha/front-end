7bc417cbd7c435ab2990d7f2de4028a44e49c455
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-21 13:07:16 -0700

    styled the potluck cards; moved stylesheet import to index.jsx

--- src/components/elements/PotluckCard/PotluckCard.jsx
+++ src/components/elements/PotluckCard/PotluckCard.jsx
@@ -11,7 +11,9 @@ export const PotluckCard = (props) => {
       <p className="potluck-card__description">Location: {location}</p>
       <p className="potluck-card__description">Date: {date}</p>
       <p className="potluck-card__description">Time: {time}</p>
-      <Link to={`/potluck/view/${organizer}/${potluck_id}`}>View</Link>
+      <Link className="button" to={`/potluck/view/${organizer}/${potluck_id}`}>
+        View
+      </Link>
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 // Components
 import App from "./App";
+// Styles
+import "./index.css";
 
 // Render the frontend (in strict mode w/ SPA Routing + Redux)
 ReactDOM.render(
