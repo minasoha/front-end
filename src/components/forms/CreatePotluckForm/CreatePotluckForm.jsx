303531f97de367e5977eac2aa5dd5584a598bea3
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-20 17:27:23 -0700

    edited the create potluck form to store data in local state in the format the API is expecting; inputs are controlled

--- src/components/forms/CreatePotluckForm/CreatePotluckForm.jsx
+++ src/components/forms/CreatePotluckForm/CreatePotluckForm.jsx
@@ -1,42 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
-// import axiosWithAuth from "../../../utilities";
+import { axiosWithAuth } from "./../../../utilities";
+
+// Initial State Data
+const initialPotluckValues = {
+  potluck_name: "",
+  location: "",
+  date: "",
+  time: "",
+};
 
 export const CreatePotluckForm = () => {
+  // Destructuring
   const { push } = useHistory();
 
-  // useEffect(
-  //   axiosWithAuth()
-  //     .post("")
-  //     .then((resp) => {
-  //       console.log(resp);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     })
-  // );
+  // State Initialization
+  const [potluckValues, setPotluckValues] = useState(initialPotluckValues);
 
+  // Event Handlers
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPotluckValues({
+      ...potluckValues,
+      [name]: value,
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    push("/dashboard");
+    console.log(potluckValues);
+    // push("/dashboard");
   };
 
+  // Markup
   return (
     <form className="form" onSubmit={handleSubmit}>
       <label className="form__label">
         Event Name:
-        <input className="form__text-field" type="text" name="event-name" />
+        <input
+          className="form__text-field"
+          type="text"
+          name="potluck_name"
+          onChange={handleChange}
+          value={potluckValues.potluck_name}
+        />
       </label>
       <label className="form__label">
         Location:
-        <input className="form__text-field" type="text" name="location" />
+        <input
+          className="form__text-field"
+          type="text"
+          name="location"
+          onChange={handleChange}
+          value={potluckValues.location}
+        />
       </label>
       <label className="form__label">
-        Date/Time:
+        Date (mm-dd-yy):
         <input
-          className="form__date-field"
-          type="datetime-local"
-          name="dateTime"
+          className="form__text-field"
+          type="text"
+          name="date"
+          onChange={handleChange}
+          value={potluckValues.date}
+        />
+      </label>
+      <label className="form__label">
+        Time (hh:mm:ss):
+        <input
+          className="form__text-field"
+          type="text"
+          name="time"
+          onChange={handleChange}
+          value={potluckValues.time}
         />
       </label>
       <button onSubmit={handleSubmit} className="button">
